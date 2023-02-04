8804495393bc7c1dfd2d9c0b72c03b7009748c2e
Flaviu8 <flavius_flaviu93@yahoo.com>
2023-02-04 23:19:12 +0000

    commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import  ScoreById  from './routes/score/score.js'
 import  AnswerById  from './routes/answers/answers.js'
 
 
-const port = 5432;
+const port = 3008;
 const app = express();
 app.use(bodyParser.json())
 
