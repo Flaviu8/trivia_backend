7357ac774ff356f26e58cc9561db619487377392
Flaviu8 <flavius_flaviu93@yahoo.com>
2023-02-04 23:23:09 +0000

    change port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import  ScoreById  from './routes/score/score.js'
 import  AnswerById  from './routes/answers/answers.js'
 
 
-const port = 3008;
+const port = 8080;
 const app = express();
 app.use(bodyParser.json())
 
