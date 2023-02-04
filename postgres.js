3cc4fdfc389ba6b522fd4df1ba2f8b639a574db6
Flaviu8 <flavius_flaviu93@yahoo.com>
2023-02-04 15:41:11 +0000

    first commit

--- postgres.js
+++ postgres.js
@@ -6,12 +6,9 @@ dotenv.config()
 
 
 export const pool = new Pool({
-    database: "btrosz3r4flacazjbc5u",
-    user: "ue56sckblr5cdfagjhnh",
-    password: "BSGfqkZwAZpHaIxJFH6yP3Xmr5r59A",
-    port: 5432,
-    localhost: "btrosz3r4flacazjbc5u-postgresql.services.clever-cloud.com"
+    database: process.env.PG_DB,
+    user: process.env.PG_USERNAME,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+    localhost: process.env.PG_HOST
   })
-
-  
-
